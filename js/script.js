46a78373e950952fcf0d2e5c7681d453b01e7b4a
Anyimar <anyimardenobrega@gmail.com>
2025-04-14 23:55:32 -0400

    creacion de archivo js, con la intencion de agrfegar animacion al diseño del carucel

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const carousel = document.querySelector('.carrusel');
+const slides = document.querySelectorAll('.elemento');
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+
+let currentIndex = 0;
+let intervalTime = 5000; // cada 5 segundos
+let autoSlide = setInterval(moveToNextSlide, intervalTime);
+
+function updateCarousel() {
+carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function moveToNextSlide() {
+currentIndex = (currentIndex + 1) % slides.length;
+updateCarousel();
+}
+
+function moveToPrevSlide() {
+currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+updateCarousel();
+}
+
+// Botones manuales
+nextBtn.addEventListener('click', () => {
+moveToNextSlide();
+resetInterval();
+});
+
+prevBtn.addEventListener('click', () => {
+moveToPrevSlide();
+resetInterval();
+});
+
+function resetInterval() {
+clearInterval(autoSlide);
+autoSlide = setInterval(moveToNextSlide, intervalTime);
+}
